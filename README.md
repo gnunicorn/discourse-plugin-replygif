9fadc25ed1ecd5f7a39927b8a2a1ff2659e0a36d
cpradio <forums@cpradio.net>
2015-09-10 19:53:41 -0400

    Initial Import

---
+++ README.md
@@ -0,0 +1,15 @@
+# ReplyGif Plugin
+
+This plugin adds support for ReplyGif to quickly embed animated gifs into a topic.
+
+## Installation
+
+Clone this repository in your Discourse plugins directory, rebuild the container and you are all set.
+
+## Authors
+
+Matthew Wilkin
+
+## License
+
+GNU GPL v2
---
+++ plugin.rb
@@ -0,0 +1,12 @@
+# name: discourse-plugin-replygif
+# about: Add ReplyGIF support
+# version: 0.1
+# authors: Matthew Wilkin
+# url: https://github.com/cpradio/discourse-plugin-replygif
+
+enabled_site_setting :replygif_enabled
+
+#register_asset "javascripts/admin/templates/site_settings/setting_text.hbs"
+#register_asset "javascripts/discourse/templates/composer-help.hbs"
+
+register_asset 'stylesheets/replygif.scss'
